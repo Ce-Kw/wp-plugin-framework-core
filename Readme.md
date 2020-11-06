8e97f15a9fd811e5a295aaae3b6fac1927081195
Christoph Ehlers <ce@construktiv.de>
2020-11-06 18:56:09 +0100

    typo in readme and implement AbstractExtenderBridge for Taxonomies

--- Readme.md
+++ Readme.md
@@ -1,10 +1,12 @@
+# wp-plugin-framework-core
+*Foundation for advanced WordPress plugin development by Christoph Ehlers & Kevin Wellmann* 
 ## Developer notes
 
-### Testing
+### Test and code-coverage
 
 There are several scripts to helps You testing Your code:
 
-|Skriptname|Example|Description|
+|Scriptname|Example|Description|
 |---|---|---|
 |build:cc               | composer run build:cc                 |Generates the coverage files|
 |start&#x2011;server:cc | composer run start&#x2011;server:cc   |Run local webserver to display generated coverage files|
--- src/ContentType/Taxonomy.php
+++ src/ContentType/Taxonomy.php
@@ -2,16 +2,54 @@
 
 namespace CEKW\WpPluginFramework\Core\ContentType;
 
+use CEKW\WpPluginFramework\Core\AbstractExtenderBridge;
 use CEKW\WpPluginFramework\Core\DynamicKeyResolverTrait;
 
-class Taxonomy
+/**
+ * @method array getLabelArgs
+ * @method LabelInfo setLabelArgs
+ * @method string getLabelName
+ * @method LabelInfo setLabelName
+ * @method string getLabelSingularName
+ * @method LabelInfo setLabelSingularName
+ * @method string getLabelAddNew
+ * @method LabelInfo setLabelAddNew
+ * @method string getLabelAddNewItem
+ * @method LabelInfo setLabelAddNewItem
+ * @method string getLabelEditItem
+ * @method LabelInfo setLabelEditItem
+ * @method string getLabelNewItem
+ * @method LabelInfo setLabelNewItem
+ * @method string getLabelViewItem
+ * @method LabelInfo setLabelViewItem
+ * @method string getLabelViewItems
+ * @method LabelInfo setLabelViewItems
+ * @method string getLabelMenuName
+ * @method LabelInfo setLabelMenuName
+ * @method string getLabelParentItemColon
+ * @method LabelInfo setLabelParentItemColon
+ * @method string getLabelAllItems
+ * @method LabelInfo setLabelAllItems
+ * @method string getLabelUpdateItem
+ * @method LabelInfo setLabelUpdateItem
+ * @method string getLabelSearchItems
+ * @method LabelInfo setLabelSearchItems
+ * @method string getLabelNotFound
+ * @method LabelInfo setLabelNotFound
+ * @method string getLabelNotFoundInTrash
+ * @method LabelInfo setLabelNotFoundInTrash
+ */
+class Taxonomy extends AbstractExtenderBridge
 {
     use DynamicKeyResolverTrait;
     use MetaBoxTrait;
 
-    private LabelInfo $labelInfo;
-
     private bool $isPublic = false;
+
+    public function __construct()
+    {
+        $this->addExtend(new LabelInfo());
+    }
 
     public function getIsPublic(): bool
     {
@@ -34,18 +72,7 @@ class Taxonomy
         return [
             'public' => $this->isPublic,
             'supports' => ['title'],
-            'labels' => $this->labelInfo->getArgs()
+            'labels' => $this->getLabelArgs()
         ];
-    }
-
-    public function getLabelInfo(): LabelInfo
-    {
-        return $this->labelInfo;
-    }
-
-    public function setLabelInfo(LabelInfo $labelInfo): Taxonomy
-    {
-        $this->labelInfo = $labelInfo;
-        return $this;
     }
 }
