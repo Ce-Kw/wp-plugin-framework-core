9a52b514b255e9c16ced8521e49a85d5b723b731
Kevin Wellmann <kevin@wellmann.io>
2020-11-07 00:17:25 +0100

    add tests for DynamicKeyResolverTrait

---
+++ tests/DynamicKeyResolverTraitTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CEKW\WpPluginFramework\Tests;
+
+use CEKW\WpPluginFramework\Core\DynamicKeyResolverTrait;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+/**
+ * @covers \CEKW\WpPluginFramework\Core\DynamicKeyResolverTrait
+ */
+class DynamicKeyResolverTraitTest extends MockeryTestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testResolveKeyFromClassName($suffix, $className, $expectedResult)
+    {
+        $traitClassMock = $this->getMockForTrait(DynamicKeyResolverTrait::class, [], $className);
+        $result = $traitClassMock->resolveKeyFromClassName($suffix);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function dataProvider()
+    {
+        return [
+            ['PostType', 'BookPostType', 'book'],
+            ['PostType', 'ContactPersonPostType', 'contact-person'],
+            ['PostType', 'MyPostTypeWithSuperLongKeyPostType', 'my-post-type-with-super-long-key'],
+            ['Taxonomy', 'GenreTaxonomy', 'genre'],
+            ['Taxonomy', 'OldLocationTaxonomy', 'old-location'],
+            ['Taxonomy', 'MyCustomTaxonomyWhichIsKindaLongTaxonomy', 'my-custom-taxonomy-which-is-kinda-long'],
+        ];
+    }
+}
