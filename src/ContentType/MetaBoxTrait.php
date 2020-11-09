8362da5d9b7c2f9ef0dc649bb25cbaec16de5e2e
Kevin Wellmann <kevin@wellmann.io>
2020-11-09 18:39:28 +0100

    fix some issues with MetaBoxTrait

--- src/ContentType/MetaBoxTrait.php
+++ src/ContentType/MetaBoxTrait.php
@@ -7,14 +7,14 @@ trait MetaBoxTrait
     private string $currentId;
     private array $metaBoxes = [];
 
-    public function addMetaBox(string $title, string $context = 'normal', string $priority = 'deafult', bool $showNames = true): MetaBoxTrait
+    public function addMetaBox(string $title, string $context = 'normal', string $priority = 'default', bool $showNames = true)
     {
         $this->currentId = md5($title);
         $this->metaBoxes[$this->currentId] = [
             'id' => $this->currentId,
             'title' => $title,
             'object_types' => [$this->getKey()],
-            'context' => $context,
+            'context' => $this->showInRest && $context === 'normal' ? 'side' : $context,
             'priority' => $priority,
             'show_names' => $showNames
         ];
@@ -22,9 +22,18 @@ trait MetaBoxTrait
         return $this;
     }
 
-    public function addFields(array $fields): void
+    public function showOn(string $key, $value)
+    {
+        $this->metaBoxes[$this->currentId]['show_on'] = compact('key', 'value');
+
+        return $this;
+    }
+
+    public function addFields(array $fields)
     {
         $this->metaBoxes[$this->currentId]['fields'] = $fields;
+
+        return $this;
     }
 
     public function getMetaBoxes(): array
